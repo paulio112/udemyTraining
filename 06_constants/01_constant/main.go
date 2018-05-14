3ddba651286449dcc9a44b416505e1bc70e16788
Paul Southall <paulsouthallweb@gmail.com>
2018-05-14 22:15:32 +1000

    Starting to with declaring multiple constants and scraping of urls

---
+++ 06_constants/01_constant/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const p  = "this is a sample of a constant"
+
+func main() {
+
+	const q int = 100
+
+  fmt.Println("Constant P Value ----->", p)
+	fmt.Println("Constant q Value ----->", q)
+}
---
+++ 06_constants/02_multiple-initialization/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	PI       = 3.16
+	Language = "Golang"
+)
+
+func main() {
+	fmt.Println("hello world",PI,Language)
+	fmt.Println(PI)
+}
