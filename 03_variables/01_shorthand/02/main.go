dc94b1f0bf18281149096abb3ba03577cd5c571a
Paul Southall <paulsouthallweb@gmail.com>
2018-05-14 19:59:28 +1000

    Updated string exercise including shorthand variable declaration, initialisation and assign

---
+++ 03_variables/01_shorthand/02/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a string
+	var b string
+	var c string
+	var d string
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+
+}
---
+++ 03_variables/01_shorthand/03/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// good example of how to output a name using shorthand.
+	name := "My name is Paul"
+	fmt.Printf("%v \n", name)
+
+}
